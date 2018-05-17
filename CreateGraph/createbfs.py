dc5c04339fec90afe164c36e2df586cbc9f4e69f
firstdream01 <944187787@qq.com>
2018-05-17 08:36:01 -0700

    create bfs graph fail

--- CreateGraph/createbfs.py
+++ CreateGraph/createbfs.py
@@ -10,9 +10,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+from bidict import bidict
 
 time_start = time.time()
 
+f = open("/home/w/data/testdata/bfs.txt", "w+")
 
 g = nx.random_graphs.barabasi_albert_graph(100, 7)   # log2 n  100K nodes   (100000, 17)
 G = nx.Graph()
@@ -20,25 +22,61 @@ G = nx.Graph()
 edges = list(g.edges())
 nodes = list(g.nodes())
 
+# 首先映射边集情况
+edges_bd = bidict()
+ed2no_bd = bidict()
+
+for i in range(len(edges)):
+	if edges[i][0] > edges[i][1]:
+		edges_bd[(edges[i][0], edges[i][1])] = (edges[i][1], edges[i][0])
+		ed2no_bd[i] = (edges[i][1], edges[i][0])
+	else:
+		edges_bd[(edges[i][0], edges[i][1])] = (edges[i][0], edges[i][1])
+		ed2no_bd[i] = (edges[i][0], edges[i][1])
+
+ed2no_db = ed2no_bd.inv
+edges_db = edges_bd.inv
+
+
 for i in range(len(edges)):
 	G.add_node(i)
 
 
-
 for i in range(len(nodes)):
 	a = [n for n in g.neighbors(i)]
-	print a
+	e = []
+	for j in range(len(a)):
+		if i < a[j]:
+			e.append(ed2no_db[(i,a[j])])
+		else:
+			e.append(ed2no_db[(a[j],i)])
+
+	for m in range(len(e)):
+		for mm in range(m+1,len(e)):
+			G.add_edge(e[m], e[mm])
+
+
+b = list(nx.bfs_edges(G, 0))
+c = []
+for i in range(len(b)):
+	if b[i][0] not in c:
+		c.append(b[i][0])
+	if b[i][1] not in c:
+		c.append(b[i][1])
+
+# ans_edges = []
+for i in range(len(c)):
+	temp = edges_db[ed2no_bd[i]]
+	src = str(temp[0])
+	tar = str(temp[1])
+	s = src + '\t' + tar + '\n'
+	f.write(s)
 
 
 
 
 
-
-a = list(nx.bfs_edges(g, 1))
-
-
-
-
+# print ans_edges
 
 # nx.draw(G)
 # plt.show()
--- wjalgorithm/test.py
+++ wjalgorithm/test.py
@@ -8,64 +8,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+from bidict import bidict
 
-def SimpleBufAL(edgelist, numOfParts):
-    f = open(edgelist, "r")
-    # [[(src, dst), (src, dst),...],[()],[()]....]  每个分区对应的边集合
-    Partitions = [[] for i in range(numOfParts)]
-    # { part:set(v1,v2,...), ... }                  存储每个分区对应的点
-    vertexDic = {}
-    # { vertex:set(part1, part2,...),... }          存储每个点对应的分区
-    ver2partDic = {}
-    
-    # { vertex:degree,... }                         存储每个点对应的度信息
-    ver2degreeDic = {}
-    # {(src, tar):balance, ...}                     存储buf中每条边的平衡度
-    edgeBalance = {}
+d = bidict({1:2})
 
-    # 存储总边数
-    edgeNum = 0
-
-    # 将边转换为标准形式，存储在lines中
-    lines = f.readlines()
-    for i in range(len(lines)):
-        line = lines[i]
-        srcTar = line.strip().split()
-        src = long(srcTar[0])
-        tar = long(srcTar[1])
-        lines[i] = (src, tar)
-
-    vertexOut = {}
-    vertexIn = {}
-    vertex = []
-
-    for i in range(len(lines)):
-        src = lines[i][0]
-        tar = lines[i][1]
-        if vertexOut.has_key(src):
-            vertexOut[src] = vertexOut[src] + 1
-        else:
-            vertexOut[src] = 1
-        if vertexIn.has_key(tar):
-            vertexIn[tar] = vertexIn[tar] + 1
-        else:
-            vertexIn[tar] = 1
-        if src not in vertex:
-            vertex.append(src)
-        if tar not in vertex:
-            vertex.append(tar)
-
-    for i in range(len(vertex)):
-        if vertexIn.has_key(vertex[i]):
-            indeg = vertexIn[vertex[i]]
-        else:
-            indeg = 0
-        if vertexOut.has_key(vertex[i]):
-            outdeg = vertexOut[vertex[i]]
-        else:
-            outdeg = 0
-        print indeg, outdeg
-
-
-SimpleBufAL("/home/w/data/soc-pokec-relationships.txt", 100)
-
+print d
+print d[1]
+D = d.inv
+print D
+print D[2]
