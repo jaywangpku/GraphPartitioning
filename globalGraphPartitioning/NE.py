0172e2ed1c6a3c16544c051580c3259485e7c2c7
jiewang666 <944187787@qq.com>
2018-09-18 04:35:03 -0700

    change

--- globalGraphPartitioning/NE.py
+++ globalGraphPartitioning/NE.py
@@ -8,14 +8,16 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 f = open("/home/w/data/Wiki-Vote.txt", "r")
-G = nx.DiGraph()
+G = nx.Graph()
 for line in f:
     srcTar = line.strip().split()
     src = long(srcTar[0])
     tar = long(srcTar[1])
     G.add_edge(src, tar)
+# G = nx.random_graphs.barabasi_albert_graph(100, 7)
 P = 100
-PLEN = len(G.edges)/100
+PLEN = len(G.edges)/P
+print len(G.edges)
 
 # x = list(G.nodes)[0]
 # print x
@@ -24,48 +26,57 @@ PLEN = len(G.edges)/100
 # [[(src, dst), (src, dst),...],[()],[()]....]  每个分区对应的边集合
 Partitions = [[] for i in range(P)]
 for i in range(P):
-	core_set = set()
-	boundary_set = set()
-	while len(Partitions[i]) < PLEN:
-		diff_set = boundary_set - core_set
-		if len(diff_set) == 0:
-			print "aaaaaa"
-			x = random.randint(0, len(G.nodes)-1)
-			x = list(G.nodes)[x]
-		else:
-			print "bbbbbb"
-			diff_list = list(diff_set)
-			diff_neighbors = []
-			for j in range(len(diff_list)):
-				diff_neighbors.append(len(set(G.neighbors(diff_list[j])) - boundary_set))
-			x = 0
-			for j in range(len(diff_neighbors)):
-				if diff_neighbors[j] < diff_neighbors[x]:
-					x = j
-			x = diff_list[x]
+    core_set = set()
+    boundary_set = set()
+    while len(Partitions[i]) < PLEN:
+        # nx.draw(G, node_color = 'b', edge_color = 'r', font_size = 18, node_size = 50)
+        # plt.show()
+        diff_set = boundary_set - core_set
+        if len(diff_set) == 0:
+            print "aaaaaa"
+            x = random.randint(0, len(G.edges)-1)  # 随机从当前剩余的图中选取一点作为起始点
+            x = list(G.edges)[x][0]
+        else:
+            print "bbbbbb"
+            diff_list = list(diff_set)
+            diff_neighbors = []
+            for j in range(len(diff_list)):
+                diff_neighbors.append(len(set(G.neighbors(diff_list[j])) - boundary_set))  # 选邻居数最小的作为候选点
+            x = 0
+            for j in range(len(diff_neighbors)):
+                if diff_neighbors[j] < diff_neighbors[x]:
+                    x = j
+            x = diff_list[x]
 
-		print "x " ,x 
+        print "x " ,x
 
-		x_neighbors = list(set(G.neighbors(x)) - boundary_set)
+        x_neighbors = list(set(G.neighbors(x)) - boundary_set)
 
-		print "x_neighbors " ,len(x_neighbors)
+        print "x_neighbors " ,len(x_neighbors)
 
-		core_set.add(x)
-		boundary_set.add(x)
-		for j in range(len(x_neighbors)):
-			y = x_neighbors[j]
-			boundary_set.add(y)
-			y_neighbors = list(set(G.neighbors(y)) & boundary_set)
-			for k in range(len(y_neighbors)):
-				Partitions[i].append((y, y_neighbors[k]))
-				G.remove_edge(y, y_neighbors[k])
-				if len(Partitions[i]) > PLEN:
-					break
-			if len(Partitions[i]) > PLEN:
-				break
-		print "G.edges " ,len(G.edges)
+        core_set.add(x)
+        boundary_set.add(x)
+        for j in range(len(x_neighbors)):
+            y = x_neighbors[j]
+            boundary_set.add(y)
+            y_neighbors = list(set(G.neighbors(y)) & boundary_set)
+            for k in range(len(y_neighbors)):
+                Partitions[i].append((y, y_neighbors[k]))
+                G.remove_edge(y, y_neighbors[k])
+                if len(Partitions[i]) > PLEN:
+                    break
+            if len(Partitions[i]) > PLEN:
+                break
+        # G.remove_node(x)
 
-		print "\n"
-
+        print "G.edges " ,len(G.edges)
+        print "\n"
+        if len(G.edges) == 0:
+            break
+    if len(G.edges) == 0:
+        break
+ans = 0
 for i in range(P):
-	print len(Partitions[i])
+    print len(Partitions[i])
+    ans = ans + len(Partitions[i])
+print ans
--- globalGraphPartitioning/test.py
+++ globalGraphPartitioning/test.py
@@ -7,13 +7,30 @@ import time
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.DiGraph()
-G.add_edge(1, 2)
-G.add_edge(2, 3)
-G.remove_edge(1, 2)
-G.clear()
+# G = nx.random_graphs.barabasi_albert_graph(20, 4)	# 生成的是无向图
+# G = nx.DiGraph()
+# G.add_edge(1, 2)
+# G.add_edge(2, 3)
 
-nx.draw(G)
-plt.show()
+# print len(G.edges)
+
+# print len(list(G.neighbors(2)))
+# nx.draw(G, node_color = 'b', edge_color = 'r', font_size = 18, node_size = 50)
+# plt.show()
 
 
+# 将图改成无向图
+# f = open("/home/w/data/Wiki-Vote.txt", "r")
+# f1 = open("/home/w/data/Wiki-Vote-undirected.txt", "w+")
+# G = nx.Graph()
+# for line in f:
+#     srcTar = line.strip().split()
+#     src = long(srcTar[0])
+#     tar = long(srcTar[1])
+#     G.add_edge(src, tar)
+# edges = list(G.edges)
+# for i in range(len(edges)):
+# 	src = edges[i][0]
+# 	tar = edges[i][1]
+# 	s = str(src) + "\t" + str(tar) + "\n"
+# 	f1.write(s)
